1e2ce1d20be41d1e7621f19cdbe356f5f8f7e93d
2hapu <yonzn@naver.com>
2023-05-24 13:50:45 +0900

    feat: 템플릿 메소드 패턴 적용 NUserDao, DUserDao 생성

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,20 +7,21 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public class UserDao {
+public abstract class UserDao {
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Map<String, String> env = getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(
-                dbHost, dbUser, dbPassword
-        );
-        return conn;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+//    {
+//        Map<String, String> env = getenv();
+//        String dbHost = env.get("DB_HOST");
+//        String dbUser = env.get("DB_USER");
+//        String dbPassword = env.get("DB_PASSWORD");
+//
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection conn = DriverManager.getConnection(
+//                dbHost, dbUser, dbPassword
+//        );
+//        return conn;
+//    }
     public void add(User user) throws SQLException, ClassNotFoundException {
         Connection conn = getConnection();
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
@@ -57,7 +58,7 @@ public class UserDao {
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new NUserDao();
         User user = new User();
         user.setId("3");
         user.setName("yongjon");
